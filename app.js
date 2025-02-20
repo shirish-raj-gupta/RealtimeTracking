8d64ebe9f52586e579abedf7eb6df2ba631cc451
shirish-raj-gupta <codersrg@gmail.com>
2025-02-20 11:19:43 +0530

    Normalize line endings

---
+++ app.js
@@ -0,0 +1,30 @@
+const express =require('express');
+const app = express();
+const path = require('path');
+const http =require('http');
+
+const socketio = require('socket.io');
+const { log } = require('console');
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on('connection',(socket)=>{
+  socket.on('send-location',(data)=>{
+    io.emit('receive-location', {id: socket.id, ...data});
+  });
+  socket.on('disconnet,',()=>{
+    io.emit('user-disconnected', socket.id)
+  })
+  
+});
+
+app.get('/',(req,res)=>{
+  res.render('index');
+})
+
+server.listen(3000);
